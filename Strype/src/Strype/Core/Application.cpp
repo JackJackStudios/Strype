3b7ecc58e6ea7a5f810b275d952c747bcc08c176
Jack Knox <jackjackfilms1@gmail.com>
2025-07-16 10:09:43 +0100

    Remove LayerStack

--- Strype/src/Strype/Core/Application.cpp
+++ Strype/src/Strype/Core/Application.cpp
@@ -59,11 +59,11 @@ namespace Strype {
 		if (m_Config.ImGuiEnabled)
 			m_ImGuiLayer.reset();
 
-		m_LayerStack.DestroyLayers();
+		for (Layer* layer : m_LayerStack)
+			delete layer;
 
 		ScriptEngine::Shutdown();
 
-		Renderer::Shutdown();
 		Audio::Shutdown();
 	}
 
@@ -109,17 +109,18 @@ namespace Strype {
 				if (m_Config.DockspaceEnabled)
 				{
 					ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar;
+					window_flags |= ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+					window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 
 					ImGuiViewport* viewport = ImGui::GetMainViewport();
 					ImGui::SetNextWindowPos(viewport->Pos);
 					ImGui::SetNextWindowSize(viewport->Size);
 					ImGui::SetNextWindowViewport(viewport->ID);
+
 					ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
 					ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-					window_flags |= ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-					window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
 					ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
 					ImGui::Begin("DockSpace", 0, window_flags);
 
 					ImGui::PopStyleVar(3);
--- Strype/src/Strype/Core/Application.hpp
+++ Strype/src/Strype/Core/Application.hpp
@@ -2,9 +2,8 @@
 
 #include "Strype/Core/Base.hpp"
 
-#include "Strype/Core/LayerStack.hpp"
 #include "Strype/Core/Event.hpp"
-
+#include "Strype/Core/Layer.hpp"
 #include "Strype/Renderer/Renderer.hpp"
 
 #include <AGI/agi.hpp>
@@ -33,28 +32,19 @@ namespace Strype {
 		Application(const AppConfig& config);
 		virtual ~Application();
 
-		std::unique_ptr<AGI::Window>& GetWindow() { return m_Window; }
-
-		void Close();
-
 		static Application& Get() { return *s_Instance; }
 
+		void Close();
+		
 		const AppConfig& GetConfig() const { return m_Config; }
-		std::shared_ptr<AGI::ImGuiLayer> GetImGuiLayer() { return m_ImGuiLayer; }
+		std::unique_ptr<AGI::Window>& GetWindow() { return m_Window; }
 
 		void OnEvent(Event& e);
 
 		template<typename T, typename... Args>
 		Application& PushLayer(Args&&... args)
 		{
-			m_LayerStack.PushLayer(new T(std::forward<Args>(args)...));
-			return *this;
-		}
-
-		template<typename T, typename... Args>
-		Application& PushOverlay(Args&&... args)
-		{
-			m_LayerStack.PushOverlay(new T(std::forward<Args>(args)...));
+			m_LayerStack.emplace_back(new T(std::forward<Args>(args)...));
 			return *this;
 		}
 	private:
@@ -62,17 +52,18 @@ namespace Strype {
 		bool OnWindowClose(WindowCloseEvent& e);
 		bool OnWindowResize(WindowResizeEvent& e);
 		void InstallCallbacks();
+	private:
+		AppConfig m_Config;
 
 		std::unique_ptr<AGI::Window> m_Window;
-		std::shared_ptr<AGI::ImGuiLayer> m_ImGuiLayer;
-		AppConfig m_Config;
+		std::unique_ptr<AGI::ImGuiLayer> m_ImGuiLayer;
 
 		bool m_Running = true;
 		bool m_Minimized = false;
 
 		int m_StartupFrames;
 		float m_LastFrameTime = 0.0f;
-		LayerStack m_LayerStack;
+		std::vector<Layer*> m_LayerStack;
 	private:
 		static Application* s_Instance;
 
